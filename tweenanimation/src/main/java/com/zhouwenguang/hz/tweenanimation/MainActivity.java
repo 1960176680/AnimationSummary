efe514de70accd94a078607269cf74aadd2dfb7d
Sun <1960176680@qq.com>
2018-07-11 14:46:26 +0800

    补间动画

--- tweenanimation/src/main/java/com/zhouwenguang/hz/tweenanimation/MainActivity.java
+++ tweenanimation/src/main/java/com/zhouwenguang/hz/tweenanimation/MainActivity.java
@@ -2,12 +2,37 @@ package com.zhouwenguang.hz.tweenanimation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
+    ImageView imageView;
+    private Animation animation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initData();
     }
+    private void initView() {
+        button=findViewById(R.id.btn);
+        imageView=findViewById(R.id.img);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.startAnimation(animation);
+            }
+        });
+
+    }
+    private void initData() {
+        animation=AnimationUtils.loadAnimation(getApplicationContext(),R.anim.tween);
+    }
+
+
 }
