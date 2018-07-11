e5e382f9a1a88f18b53e69a71a4e5e25590eded5
Sun <1960176680@qq.com>
2018-07-11 14:41:50 +0800

    帧动画

--- frameanimation/src/main/java/com/zhouwenguang/hz/animationsummary/MainActivity.java
+++ frameanimation/src/main/java/com/zhouwenguang/hz/animationsummary/MainActivity.java
@@ -1,13 +1,33 @@
 package com.zhouwenguang.hz.animationsummary;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
+/**
+ * 动画xml资源位于：res\drawable\frame.xml
+ * 使用：
+ * AnimationDrawable animationDrawable= (AnimationDrawable) imageView.getBackground();
+ *animationDrawable.start();
+ */
 public class MainActivity extends AppCompatActivity {
-
+    ImageView imageView;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView=findViewById(R.id.image);
+        button=findViewById(R.id.btn);
+        final AnimationDrawable animationDrawable= (AnimationDrawable) imageView.getBackground();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animationDrawable.start();
+            }
+        });
     }
 }
